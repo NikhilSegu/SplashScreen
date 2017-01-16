694bc2819132c03b97ed05e0930506b644df2849
Sai Nikhil <sainikhil1213@gmail.com>
2017-01-16 20:37:23 +0530

    Add files via upload

---
+++ java/com/nick/splashscreenexample/Activity_Main.java
@@ -0,0 +1,14 @@
+package com.nick.splashscreenexample;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Activity_Main extends Activity {
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
